28b6ad785275af20346203bf488bdcc50864d44f
Pedro Minicz <pedrominicz@gmail.com>
2021-02-27 16:19:47 -0300

    feat: compile reverse polish notation

--- test_rpn.py
+++ test_rpn.py
@@ -6,11 +6,11 @@ def read(string):
         try:
             result.append(int(word))
         except:
-            assert len(word) == 1 and word in '+-*/^()'
+            assert len(word) == 1 and word in '+-*/()'
             result.append(word)
     return result
 
-precedence = {'+': 0, '-': 0, '*': 1, '/': 1, '^': 2}
+precedence = {'+': 0, '-': 0, '*': 1, '/': 1}
 
 def convert(infix):
     postfix = []
@@ -19,7 +19,7 @@ def convert(infix):
         if type(item) is int:
             postfix.append(item)
         else:
-            assert type(item) is str and len(item) == 1 and item in '+-*/^()'
+            assert type(item) is str and len(item) == 1 and item in '+-*/()'
             if item == '(':
                 stack.append(item)
             elif item == ')':
@@ -29,10 +29,55 @@ def convert(infix):
                 stack.pop()
             else:
                 while (stack and stack[-1] != '(' and
-                       precedence[stack[-1]] > precedence[item]):
+                       precedence[stack[-1]] >= precedence[item]):
                     postfix.append(stack.pop())
                 stack.append(item)
     postfix.extend(stack[::-1])
     return postfix
 
-print(convert(read(input())))
+print('.extern printf')
+print('.global _start')
+print('')
+print('.text')
+print('_start:')
+
+for instruction in convert(read(input())):
+    if type(instruction) is int:
+        print(f'        mov ${instruction}, %rax')
+        print(f'        push %rax')
+    elif instruction == '+':
+        print('        pop %rcx')
+        print('        pop %rax')
+        print('        add %rcx, %rax')
+        print('        push %rax')
+    elif instruction == '-':
+        print('        pop %rcx')
+        print('        pop %rax')
+        print('        sub %rcx, %rax')
+        print('        push %rax')
+    elif instruction == '*':
+        print('        pop %rcx')
+        print('        pop %rax')
+        print('        imul %rcx, %rax')
+        print('        push %rax')
+    elif instruction == '/':
+        print('        pop %rcx')
+        print('        pop %rax')
+        print('        cqo')
+        print('        idiv %rcx')
+        print('        push %rax')
+    else:
+        raise RuntimeError(f'unknown instruction: {instruction}')
+
+print('        lea fmt(%rip), %rdi')
+print('        pop %rsi')
+print('        xor %eax, %eax')
+print('        call printf')
+print('')
+print('        mov $60, %rax')
+print('        xor %rdi, %rdi')
+print('        syscall')
+print('')
+print('.data')
+print('fmt:')
+print('        .asciz "%lld\\n"')
