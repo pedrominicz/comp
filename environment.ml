7808baf5fe503914831246cbe491ae7bed5ff365
Pedro Minicz <pedrominicz@gmail.com>
2021-03-27 15:28:08 -0300

    feat: use `Hashtbl` instead of `Map`

--- environment.ml
+++ environment.ml
@@ -1,40 +1,37 @@
-module Environment = Map.Make(String)
-
 type t =
-  | Env of Value.t Environment.t * t option
+  | Env of (string, Value.t) Hashtbl.t * t option
 
-let env : t ref =
-  let clock =
-    Value.Callable (0, "clock", fun _ -> Value.Number (Sys.time ())) in
-  ref (Env (Environment.singleton "clock" clock, None))
+let empty : t = Env (Hashtbl.create 32, None)
+
+let env : t ref = ref empty
 
 let push () =
-  env := Env (Environment.empty, Some !env)
+  env := Env (Hashtbl.create 32, Some !env)
 
 let pop () =
   env := match !env with Env (current, Some env) -> env | _ -> !env
 
 let define name value = 
-  let current, previous =
+  let current =
     match !env with
-    | Env (current, previous) -> (current, previous) in
-  env := Env (Environment.add name value current, previous)
+    | Env (current, previous) -> current in
+  Hashtbl.add current name value
 
 let assign name value k =
   let rec loop = function
     | Env (current, previous) ->
-        if Environment.mem name current
-          then Env (Environment.add name value current, previous)
+        if Hashtbl.mem current name
+          then Hashtbl.add current name value
           else
             match previous with
-            | Some env -> Env (current, Some (loop env))
+            | Some env -> loop env
             | None -> k () in
-  env := loop !env
+  loop !env
 
 let get name k =
   let rec loop = function
     | Env (current, previous) ->
-        match Environment.find_opt name current with
+        match Hashtbl.find_opt current name with
         | Some value -> value
         | None ->
             match previous with
